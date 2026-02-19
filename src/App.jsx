b5c42970ad421c49d0fe00762a41457d470fb3d0
Jose Rodriguez <alberttora7@gmail.com>
2026-02-18 21:55:07 -0400

    Props en accion
    Los componentes de React utilizan Props para comunicarse entre ellos.
    Puedes pasarle información de un componente padre al hijo por medio estos props.

--- src/App.jsx
+++ src/App.jsx
@@ -17,8 +17,12 @@ const [ data, setData ] = useState(db)
 
         <div className="row mt-5">
            
-            {data.map( ()=> (
-              <Guitar />
+            {data.map( (guitar) => (
+              <Guitar 
+                key={guitar.id}
+                guitar={guitar}
+                
+              />
             ))}
             
         </div>
