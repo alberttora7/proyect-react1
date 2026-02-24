8b219f1112bc311af0e7e77680aaf11bc2045915
Jose Rodriguez <alberttora7@gmail.com>
2026-02-23 22:02:37 -0400

    Eventos con JSX

--- src/components/Guitar.jsx
+++ src/components/Guitar.jsx
@@ -1,7 +1,12 @@
 
 export default function Guitar({guitar}) {
 
-    const { name, image,description, price } = guitar
+
+    const { id, name, image,description, price } = guitar
+
+    const handleClick = () => {
+        console.log(`clic a la guitarra ${name}`)
+    }
   return (
    <div className="col-md-6 col-lg-4 my-4 row align-items-center">
                 <div className="col-4">
@@ -14,6 +19,7 @@ export default function Guitar({guitar}) {
                     <button 
                         type="button"
                         className="btn btn-dark w-100"
+                        onClick={() => handleClick(guitar)}
                     >Agregar al Carrito</button>
                 </div>
             </div>
